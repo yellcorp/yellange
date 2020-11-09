8fcf58c0482bb47636edffd24cd4cf54cdf1dff2
Jim Boswell <jimb@yellcorp.org>
2020-11-09 14:53:14 -0800

    Add a test file that imports everything
    This checks that the entire library can at least be built, and gives us
    a complete picture of test coverage

---
+++ test/import_and_coverage.test.js
@@ -0,0 +1,5 @@
+// This makes sure the code coverage sees the whole picture. Also has the side
+// effect of making sure everything can at least be imported.
+
+// eslint-disable-next-line no-unused-vars
+import * as yellange from '..';
