73a1da6257b1f4849ed2c3aee3a561493de7e3b9
Jim Boswell <jimb@yellcorp.org>
2020-11-08 18:31:04 -0800

    Add webpack for running tests in browser

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const { join, resolve, sep } = require('path');
+const { readdirSync } = require('fs');
+
+function isTestFile(name) {
+  return name && name[0] !== '.' && /\.js$/.test(name);
+}
+
+function enumerateTests(root) {
+  const paths = [];
+  const q = [root];
+
+  while (q.length > 0) {
+    const dir = q.shift();
+    const entries = readdirSync(dir, { withFileTypes: true });
+    for (let entry of entries) {
+      if (!entry.isSymbolicLink()) {
+        const entryPath = join(dir, entry.name);
+        if (entry.isDirectory()) {
+          q.push(entryPath);
+        } else if (entry.isFile()) {
+          if (isTestFile(entry.name)) {
+            paths.push('.' + sep + entryPath);
+          }
+        }
+      }
+    }
+  }
+  return paths;
+}
+
+const inputFiles = enumerateTests('test');
+
+console.log({ inputFiles });
+
+module.exports = {
+  entry: inputFiles,
+  output: {
+    filename: 'unit-tests.js',
+    path: resolve(__dirname, 'bundle'),
+  },
+};
